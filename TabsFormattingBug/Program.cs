311ce623e31867ed9ec5ffde2c088f84bd44ec82
Lauri Naulapää <lauri.naulapaa@circles.fi>
2022-02-16 15:04:11 +0200

    Switched indentation to spaces

--- TabsFormattingBug/Program.cs
+++ TabsFormattingBug/Program.cs
@@ -1,22 +1,22 @@
 ﻿namespace TabsFormattingBug
 {
-	/// <summary>
-	/// Test comment
-	/// </summary>
-	public static class Program
-	{
-		public const string Dummy = "";
+    /// <summary>
+    /// Test comment
+    /// </summary>
+    public static class Program
+    {
+        public const string Dummy = "";
 
-		/// <summary>
-		/// Another comment
-		/// </summary>
-		/// <param name="args"></param>
-		public static void Main()
-		{
-			Console.WriteLine("Hello");
+        /// <summary>
+        /// Another comment
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main()
+        {
+            Console.WriteLine("Hello");
 
-			// yet another comment
-			Console.WriteLine("World!");
-		}
-	}
+            // yet another comment
+            Console.WriteLine("World!");
+        }
+    }
 }
